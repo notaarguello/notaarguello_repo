067b637e0805734b970eb68f56d708517b5c2f2f
notaarguello <andresjarguello@gmail.com>
2024-06-07 14:35:00 -0300

    check merge status

--- sh_tools/git_helpers.sh
+++ sh_tools/git_helpers.sh
@@ -1,20 +1,11 @@
 #!bin/bash
 
 
-# Function to check if PR is merged
-check_pr_merged() {
-  local pr_number=$1
-  local repo=$2
-  
-  gh_output=$(gh pr view $pr_number --repo $repo --json merged --jq '.merged')
-  echo "$gh_output"
-}
-
 # Function to get the last PR for a user
 getLastPrsFromUser() {
 
     local username=$1
-    local repo=$2
+    local repo_with_owner=$2
 
     if ! command -v gh &> /dev/null; then
         echo "GitHub CLI (gh) is not installed. Please install it first."
@@ -24,25 +15,25 @@ getLastPrsFromUser() {
     # Specify the GitHub username for which you want to list open pull requests
 
     # List open pull requests for the specified user in the current repository
-    gh_output=$(gh pr list --author "${username}" --repo "${repo}" --state "open")
+    gh_output=$(gh pr list --author "${username}" --repo "${repo_with_owner}" --state "open")
     echo "$gh_output"
 }
 
 # Function to get the commit SHA of a PR
 get_pr_commit_sha() {
   local pr_number=$1
-  local repo=$2
+  local repo_with_owner=$2
 
-  gh_output=$(gh pr view $pr_number --repo $repo --json commits --jq '.commits[-1].oid')
+  gh_output=$(gh pr view $pr_number --repo $repo_with_owner --json commits --jq '.commits[-1].oid')
   echo "$gh_output"
 }
 
 # Function to list names of completed workflow runs for a commit SHA
 list_completed_workflow_names_for_commit() {
   local commit_sha=$1
-  local repo=$2
+  local repo_with_owner=$2
 
-  gh_output=$(gh run list --repo $repo --commit $commit_sha --json name,conclusion,status --jq '.[] | select(.status == "completed") | .name')
+  gh_output=$(gh run list --repo $repo_with_owner --commit $commit_sha --json name,conclusion,status --jq '.[] | select(.status == "completed") | .name')
   echo "$gh_output"
 }
 
@@ -67,8 +58,8 @@ get_current_branch() {
 # Function to get the PR number for the current branch
 get_pr_number_for_branch() {
   local branch_name=$1
-  local repo=$2
-  gh_output=$(gh pr list --repo $repo --head $branch_name --json number --jq '.[0].number')
+  local repo_with_owner=$2
+  gh_output=$(gh pr list --repo $repo_with_owner --head $branch_name --json number --jq '.[0].number')
   echo "$gh_output"
 }
 
@@ -98,4 +89,40 @@ set_gh_aux_env_vars() {
     echo "GH_CURR_BRANCH_PR_NUMBER=$GH_CURR_BRANCH_PR_NUMBER"
     echo "GH_CURR_BRANCH_SHA=$GH_CURR_BRANCH_SHA"
     echo "GH_REPO_ROOT=$GH_REPO_ROOT"
+}
+
+# Function to check if PR is merged
+check_pr_merged() {
+  local pr_number=$1
+  local repo_with_owner=$2
+
+  gh_output=$(gh pr view $pr_number --repo $repo_with_owner --json merged --jq '.merged')
+  echo "$gh_output"
+}
+
+# Function to check if PR is merged every 2 mins and emit a notification when done
+watch_pr_merged_and_notify() {
+    local pr_number=$1
+    local repo_with_owner=$2
+
+    local max_time=$((40 * 60))
+    local interval=120
+    local elapsed_time=0
+
+    while [ $elapsed_time -lt $max_time ]; do
+        local merged_status=$(check_pr_merged $pr_number $repo_with_owner)
+        if [ "$merged_status" == "true" ]; then
+            # Emit a sound alert
+            afplay /System/Library/Sounds/Glass.aiff
+            say "The pull request ${pr_number} has been merged."
+            return 0
+        else
+            echo "The pull request #$pr_number has not been merged. Checking again in 2 minutes..."
+            sleep $interval
+            elapsed_time=$((elapsed_time + interval))
+        fi
+    done
+
+    say "The pull request #$pr_number has not been merged after 40 minutes."
+    return 1
 }
